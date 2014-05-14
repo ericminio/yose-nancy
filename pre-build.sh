3bf13907b57191c1c84571546270a0fd651e8e81
ericminio <eric.mignot@gmail.com>
2014-05-14 07:40:02 -0400

    inline pre-build.sh into .travis.yml

--- pre-build.sh
+++
@@ -1,5 +0,0 @@
-wget "http://download.mono-project.com/archive/3.2.0/macos-10-x86/MonoFramework-MDK-3.2.0.macos10.xamarin.x86.pkg" -o packages/mono-3.2.0.pkg
-
-sudo installer -pkg packages/mono-3.2.0.pkg -target /
-
-
