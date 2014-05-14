4a34285cee5261aa5006029eedbe37bbf299ad07
ericminio <eric.mignot@gmail.com>
2014-05-14 07:05:46 -0400

    Enable travis

---
+++ nunit.sh
@@ -0,0 +1,3 @@
+xbuild
+export MONO_PATH=/Library/Frameworks/Mono.Framework/Versions/3.2.0/lib/mono/4.5
+mono /Library/Frameworks/Mono.Framework/Versions/3.2.0/lib/mono/4.5/nunit-console.exe  Tests/bin/Debug/Tests.dll
---
+++ pre-build.sh
@@ -0,0 +1,5 @@
+wget "http://download.mono-project.com/archive/3.2.0/macos-10-x86/MonoFramework-MDK-3.2.0.macos10.xamarin.x86.pkg" -o packages/mono-3.2.0.pkg
+
+sudo installer -pkg packages/mono-3.2.0.pkg -target /
+
+
